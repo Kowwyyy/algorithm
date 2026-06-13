df9dd3239220d020b2d4cb6cb7da73370994cd07
Kowwyyy <morozovaoksana34@gmail.com>
2026-06-13 11:20:44 +1000

    m

--- labs/lab_2/622.py
+++ labs/lab_2/622.py
@@ -3,23 +3,23 @@ class MyCircularQueue(object):
     def __init__(self, k):
         self.queue = [0] * k   
         self.front = 0          
-        self.rear = 0          
-        self.size = 0         
+        self.rear = -1          
+        self.count = 0         
         self.capacity = k      
 
     def enQueue(self, value):
         if self.isFull():
             return False
+        self.rear = (self.rear + 1) % self.capacity
         self.queue[self.rear] = value
-        self.rear = (self.rear + 1) % self.capacity  
-        self.size += 1
+        self.count += 1
         return True
 
     def deQueue(self):
         if self.isEmpty():
             return False
         self.front = (self.front + 1) % self.capacity 
-        self.size -= 1
+        self.count -= 1
         return True
 
     def Front(self):
@@ -30,10 +30,10 @@ class MyCircularQueue(object):
     def Rear(self):
         if self.isEmpty():
             return -1
-        return self.queue[(self.rear - 1) % self.capacity]
+        return self.queue[self.rear]
 
     def isEmpty(self):
-        return self.size == 0
+        return self.count == 0
 
     def isFull(self):
-        return self.size == self.capacity
+        return self.count == self.capacity
